043b8114521a780c51cfdced14cd3e623f7365b6
Robert Kim <rkty13@gmail.com>
2015-02-03 23:35:17 -0500

    toggle functional

--- app/src/main/java/io/robertkim/flashlight/MainActivity.java
+++ app/src/main/java/io/robertkim/flashlight/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,18 +12,27 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 
     private Button toggleFlashlight;
-    private Camera cam = null;
-    private Parameters params = null;
+    private Camera cam;
+    Parameters params;
+    private boolean isOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            cam = Camera.open();
+            params = cam.getParameters();
+        } catch (RuntimeException e) {
+            Log.e("Camera Error", e.getMessage());
+        }
+
         toggleFlashlight = (Button)findViewById(R.id.toggle_button);
         toggleFlashlight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cam == null) {
+                if (!isOn) {
                     lightOn();
                 } else {
                     lightOff();
@@ -32,11 +42,11 @@ public class MainActivity extends Activity {
     }
 
     private void lightOn() {
-        cam = Camera.open();
         params = cam.getParameters();
         params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
         cam.setParameters(params);
         cam.startPreview();
+        isOn = true;
     }
 
     private void lightOff() {
@@ -44,7 +54,6 @@ public class MainActivity extends Activity {
         params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
         cam.setParameters(params);
         cam.stopPreview();
-        cam = null;
-        params = null;
+        isOn = false;
     }
 }
