5c8a11727751df819b558318efc2a7fb72bed403
Robert <rkty13@gmail.com>
2015-02-11 09:26:55 -0500

    what am i doing

--- app/src/main/java/io/robertkim/flashlight/MainActivity.java
+++ app/src/main/java/io/robertkim/flashlight/MainActivity.java
@@ -1,61 +1,36 @@
 package io.robertkim.flashlight;
 
-import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     private Button toggleFlashlight;
     private Camera cam;
     private Parameters params;
     private boolean isOn = false;
-    private static final String KEY_TOGGLE = "camera";
+    private FlashlightFragment flashlightFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            cam = Camera.open();
-            params = cam.getParameters();
-        } catch (RuntimeException e) {
-            Log.e("Camera Error", e.getMessage());
-        }
+        FragmentManager fm = getSupportFragmentManager();
+        flashlightFragment = (FlashlightFragment) fm.findFragmentByTag("cam");
 
-        toggleFlashlight = (Button)findViewById(R.id.toggle_button);
-        toggleFlashlight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!isOn) {
-                    lightOn();
-                } else {
-                    lightOff();
-                }
-            }
-        });
-        initializeCam();
-        if (savedInstanceState != null) {
-            isOn = savedInstanceState.getBoolean(KEY_TOGGLE);
-            if (isOn) {
-                lightOn();
-            }
-        } else {
-            isOn = false;
+        if (flashlightFragment == null) {
+            flashlightFragment = new FlashlightFragment();
+            fm.beginTransaction().add(flashlightFragment, "cam").commit();
+            flashlightFragment.setCamera();
         }
     }
-
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putBoolean(KEY_TOGGLE, isOn);
-    }
-
+    
     private void lightOn() {
         params = cam.getParameters();
         params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
