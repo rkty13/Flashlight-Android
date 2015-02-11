6faed2ce5b24c406d0a25e2a71415876fef7ec4c
Robert <rkty13@gmail.com>
2015-02-11 10:10:26 -0500

    finally fixed

--- app/src/main/java/io/robertkim/flashlight/FlashlightFragment.java
+++ app/src/main/java/io/robertkim/flashlight/FlashlightFragment.java
@@ -3,6 +3,10 @@ package io.robertkim.flashlight;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 /**
@@ -10,22 +14,57 @@ import android.widget.Button;
 */
 public class FlashlightFragment extends Fragment {
 
-    private Button toggleFlashlight;
     private Camera cam;
     private Camera.Parameters params;
+    private Button toggle;
     private boolean isOn = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRetainInstance(true);
+        initializeCam();
     }
 
-    public void setCamera(Camera cam) {
-        this.cam = cam;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_light, parent, false);
+        toggle = (Button) v.findViewById(R.id.button_toggle);
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isOn) {
+                    lightOff();
+                } else {
+                    lightOn();
+                }
+            }
+        });
+        return v;
     }
 
-    public Camera getCamera() {
-        return cam;
+    private void initializeCam() {
+        try {
+            cam = Camera.open();
+            params = cam.getParameters();
+        } catch (RuntimeException e) {
+            Log.e("Camera Error", e.getMessage());
+        }
+    }
+
+    private void lightOn() {
+        params = cam.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        cam.setParameters(params);
+        cam.startPreview();
+        isOn = true;
+    }
+
+    private void lightOff() {
+        params = cam.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        cam.setParameters(params);
+        cam.stopPreview();
+        isOn = false;
     }
 }
--- app/src/main/java/io/robertkim/flashlight/MainActivity.java
+++ app/src/main/java/io/robertkim/flashlight/MainActivity.java
@@ -1,19 +1,11 @@
 package io.robertkim.flashlight;
 
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
-import android.widget.Button;
 
 public class MainActivity extends FragmentActivity {
 
-    private Button toggleFlashlight;
-    private Camera cam;
-    private Parameters params;
-    private boolean isOn = false;
     private FlashlightFragment flashlightFragment;
 
     @Override
@@ -22,84 +14,15 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
 
         FragmentManager fm = getSupportFragmentManager();
-        flashlightFragment = (FlashlightFragment) fm.findFragmentByTag("cam");
+        flashlightFragment = (FlashlightFragment) fm.findFragmentById(R.id.fragmentContainer);
 
         if (flashlightFragment == null) {
             flashlightFragment = new FlashlightFragment();
-            fm.beginTransaction().add(flashlightFragment, "cam").commit();
-            flashlightFragment.setCamera();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, flashlightFragment)
+                    .commit();
+
         }
-    }
-    
-    private void lightOn() {
-        params = cam.getParameters();
-        params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-        cam.setParameters(params);
-        cam.startPreview();
-        isOn = true;
-    }
 
-    private void lightOff() {
-        params = cam.getParameters();
-        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-        cam.setParameters(params);
-        cam.stopPreview();
-        isOn = false;
     }
-
-    private void initializeCam() {
-        try {
-            cam = Camera.open();
-            params = cam.getParameters();
-        } catch (RuntimeException e) {
-            Log.e("Camera Error", e.getMessage());
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        cam.release();
-        cam = null;
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        cam.release();
-        cam = null;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (cam == null) {
-            initializeCam();
-        }
-    }
-
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        cam.release();
-//        cam = null;
-//    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (cam == null) {
-            initializeCam();
-        }
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        if (cam == null) {
-            initializeCam();
-        }
-    }
-
 }
-//Men
